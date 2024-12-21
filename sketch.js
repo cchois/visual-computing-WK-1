d0c5f82fe60ce75c87849cb1c61b5df05d5c54c5
cchois <94801991+cchois@users.noreply.github.com>
2024-12-20 22:52:53 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,19 @@
 var ship;
-var alien;
 var lasers =[];
-var alienLasers = [];
-var alienLaserCooldown;
 var laserSound;
 var asteroids = [];
-var  gameFont;
-var gameState ='MENU';
-var score = 0
+//var  gameFont;
 
 function preload() {
-  laserSound = loadSound('laser.wav');
-  hitSound = loadSound('hit.m4a');
-  collisionSound = loadSound ('bang_lg.wav');
-  gameFont = loadFont('PressStart2P-Regular.ttf');
+  laserSound = loadSound('audio/laser.wav');
+  hitSound = loadSound('audio/hit.m4a');
+  collisionSound = loadSound ('audio/bang_lg.wav');
+  //gameFont = loadFont('PressStart2P-Regular.ttf');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(60);
   ship = new Ship();
-  alien = new Alien();
-  alienLaserCooldown = 60;
   for (let i = 0; i < 5; i++) {
     asteroids.push(new Asteroid());
   }
@@ -29,41 +21,25 @@ function setup() {
 
 function draw() {
   background(0);
-   if (gameState === "MENU") {
+   /*if (gameState === "MENU") {
     drawMenu();
   } else if (gameState === "PLAYING") {
     playGame();
   } else if (gameState === "GAMEOVER") {
     drawGameOver();
-  }
-
-
-}
-
-
-function playGame(){
-    ship.show();
+  }*/
+  
+  ship.show();
   ship.update();
-  alien.show();
-  alien.update();
-
-  for (var i = lasers.length - 1 ; i  >= 0; i--){
+  for (var i = lasers.length - 1; i >= 0; i--){
     lasers[i].render();
     lasers[i].update();
-    //por alguna razon, si mando este codigo, se manda game over.
-    //si alguien puede ayudarme estaria profundamente agradecido.
-
     if (lasers[i] && lasers[i].offscreen()){
       lasers.splice(i, 1);
-
-    }else if (alien.isActive && lasers[i].hits(alien)) {
-      hitSound.play();
-      alien.restartValues();
-      lasers.splice(i, 1);
     }else{
+
     for(var j = asteroids.length - 1; j >= 0; j--){
       if (lasers[i].hits(asteroids[j])){
-        score += 10;
         if (asteroids[j].r > 20){
           var newAsteroids = asteroids[j].breakup();
           asteroids = asteroids.concat(newAsteroids);
@@ -71,99 +47,36 @@ function playGame(){
         asteroids.splice(j, 1);
         lasers.splice(i, 1);
         break;
-        }
-      }
-    }
-  }
-
-  for (let i = asteroids.length - 1; i >= 0; i--) {
-    if (ship.hits(asteroids[i])){
-      gameState = "GAMEOVER";
-      console.log('opps');
-    }
-    if (alien.hits(asteroids[i])){
-      alien.restartValues();
-    }
-    asteroids[i].update();
-    asteroids[i].show();
-    asteroids[i].edges();
-  }
-
-  if (alien.isActive == true){
-  if (alienLaserCooldown <= 0){
-    var direction = createVector(ship.pos.x - alien.pos.x, ship.pos.y - alien.pos.y);
-    var angle = Math.atan(direction.y/direction.x);   //radians
-    if (ship.pos.x <= alien.pos.x){
-      if(ship.pos.y > alien.pos.y){
-        angle += PI;
-      }
-      else if(ship.pos.y <= alien.pos.y){
-        angle -= PI;
-      }
-    }
-
-    alienLasers.push(new Laser(alien.pos, angle));
-
-    if (!laserSound.isPlaying()) {
-      laserSound.play();
-    }
-    alienLaserCooldown = 60;
-  }
-  else{
-    alienLaserCooldown -= 1;
-  }
-  }
-
-
-
-  for (var i = alienLasers.length - 1 ; i  >= 0; i--){
-    alienLasers[i].render();
-    alienLasers[i].update();
-    if (alienLasers[i].hits(ship)){
-      gameState = "GAMEOVER";
-      console.log('opps');
-    }
-    if (alienLasers[i] && alienLasers[i].offscreen()){
-      alienLasers.splice(i, 1);
-    }else{
-
-    for(var j = asteroids.length - 1; j >= 0; j--){
-      if (alienLasers[i].hits(asteroids[j])){
-        if (asteroids[j].r > 20){
-          var newAsteroids = asteroids[j].breakup();
-          asteroids = asteroids.concat(newAsteroids);
-        }
-        asteroids.splice(j, 1);
-        alienLasers.splice(i, 1);
-        break;
       }
     }}
   }
 
-   drawScore();
-  
+  for (let i = asteroids.length - 1; i >= 0; i--) {
+        if (ship.hits(asteroids[i])){
+          //gameState = "GAMEOVER";
+          console.log('opps');
+
+        }
+        asteroids[i].update();
+        asteroids[i].show();
+        asteroids[i].edges();
+    }
+   //drawScore();
 }
 
+
 function keyReleased() {
-    if (keyIsDown(UP_ARROW) && (keyIsDown(LEFT_ARROW) == false || keyIsDown(RIGHT_ARROW) == false)){
-      ship.setRotation(0);
-    }
-    else if (keyIsDown(UP_ARROW) == false && (keyIsDown(LEFT_ARROW)|| keyIsDown(RIGHT_ARROW))){
-      ship.boosting(false);
-    }
-    else if (keyIsDown(UP_ARROW) == false&& (keyIsDown(LEFT_ARROW) == false || keyIsDown(RIGHT_ARROW) == false)){
-      ship.setRotation(0);
-      ship.boosting(false);
-    }
+    ship.setRotation(0);
+    ship.boosting(false)
 }
 
 function keyPressed() {
 
-  if (gameState === "MENU" && keyCode === ENTER) {
+  /*if (gameState === "MENU" && keyCode === ENTER) {
     startGame();
   } else if (gameState === "GAMEOVER" && keyCode === ENTER) {
     resetGame();
-  } else if (gameState === "PLAYING")
+  } else if (gameState === "PLAYING")*/ 
   if (key == ' '){
     lasers.push(new Laser(ship.pos, ship.heading));
 
@@ -172,17 +85,13 @@ function keyPressed() {
     }
 
 
-  }if (keyCode == RIGHT_ARROW){
-    if (keyIsDown(LEFT_ARROW) == false){
-      ship.setRotation(PI/36);
-    }
+  }else if (keyCode == RIGHT_ARROW){
+      ship.setRotation(0.1);
 
-  }if (keyCode == LEFT_ARROW){
-    if (keyIsDown(RIGHT_ARROW) == false){
-      ship.setRotation(-PI/36);
-    }
+  }else if (keyCode == LEFT_ARROW){
+      ship.setRotation(-0.1);
 
-  }if (keyCode == UP_ARROW){
+  }else if (keyCode == UP_ARROW){
     ship.boosting(true);
 
   }
@@ -190,8 +99,8 @@ function keyPressed() {
 
 
 
-function drawMenu() {
-
+/*function drawMenu() {
+  
   background(0);
   for (let i = asteroids.length - 1; i >= 0; i--) {
     asteroids[i].update();
@@ -199,18 +108,18 @@ function drawMenu() {
     asteroids[i].edges();
   }
   fill(255);
-  textFont( gameFont);
+  textFont( gameFont); 
   textAlign(CENTER);
   textSize(32);
   text("ASTEROIDS", width / 2, height / 2 - 40);
-  textSize(16);
+  textSize(16); 
   text("Press ENTER to Start", width / 2, height / 2 + 20);
 }
 
 
 function drawGameOver() {
   fill(255);
-  textFont( gameFont);
+  textFont( gameFont); 
   textAlign(CENTER);
   textSize(32);
   text("GAME OVER", width / 2, height / 2 - 40);
@@ -222,7 +131,7 @@ function drawGameOver() {
 
 function drawScore() {
   fill(255);
-  textFont( gameFont); 
+  textFont( gameFont); /
   textAlign(LEFT);
   textSize(16);
   text(`Score: ${score}`, 10, 20);
@@ -235,4 +144,4 @@ function startGame() {
 
 function resetGame() {
   gameState = "MENU";
-}
+}*/
